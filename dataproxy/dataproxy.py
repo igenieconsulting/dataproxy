6e892f0a20dad666fb324e475fb7abb170669c8b
iGenie Consulting <info@igenieconsulting.com>
2018-09-05 19:34:29 +0000

    First commit

--- dataproxy/dataproxy.py
+++ dataproxy/dataproxy.py
@@ -1,11 +1,13 @@
-from google.appengine.ext.webapp.util import run_wsgi_app
+#from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 from app import JsonpDataProxy
+import webapp2
 
 application = JsonpDataProxy(3000000)
 
 def main():
-    run_wsgi_app(application)
+    from paste import httpserver
+    httpserver.serve(application, host='127.0.0.1', port='8000')
 
 if __name__ == "__main__":
     main()
